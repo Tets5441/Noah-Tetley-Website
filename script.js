8573d52b25d7d03ac38a7a720ed29f5e5ec7810c
Tets5441 <tetley640@gmail.com>
2019-09-26 14:41:24 +0100

    updates to menu with issues

--- script.js
+++ script.js
@@ -18,8 +18,8 @@ function scrollFunction() {
 $(document).ready(function(){
   $('.single-item').slick({
     infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToShow: 1,
+  slidesToScroll: 1
   });
 });
 
